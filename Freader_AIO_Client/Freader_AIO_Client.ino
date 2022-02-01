00712c38d51b98d1c75bd8406ad8418b34904b96
Mike Vanis <hello@mikevanis.com>
2022-02-01 16:30:52 +0000

    fix client espnow

--- Freader_AIO_Client/Freader_AIO_Client.ino
+++ Freader_AIO_Client/Freader_AIO_Client.ino
@@ -9,30 +9,47 @@ typedef struct message_struct {
 
 message_struct msg;
 
+#define LED_BUILTIN 2
+
+void onSent(uint8_t *mac_addr, uint8_t sendStatus) {
+  Serial.println("Status:");
+  Serial.println(sendStatus);
+}
+
 void setup() {
+  WiFi.mode(WIFI_STA);
+  
   Serial.begin(115200);
-  if (esp_now_init() == ERR_OK) {
-    Serial.println("ESPNow Init Success");
-  }
-  else {
-    Serial.println("ESPNow Init Failed");
-    ESP.restart();
+  if (esp_now_init() != 0) {
+    Serial.println("Problem during ESP-NOW init");
+    return;
   }
 
   esp_now_set_self_role(ESP_NOW_ROLE_CONTROLLER);
   esp_now_add_peer(globalMac, ESP_NOW_ROLE_SLAVE, 1, NULL, 0);
+  esp_now_register_send_cb(onSent);
+
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
   sendTag(random(0, 255), random(0, 255), random(0, 255), random(0, 255));
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(10);
+  digitalWrite(LED_BUILTIN, HIGH);
   delay(10000);
 }
 
 void sendTag(uint8_t a, uint8_t b, uint8_t c, uint8_t d) {
-  uint8_t macAddr[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
   msg.rfid[0] = a;
   msg.rfid[1] = b;
   msg.rfid[2] = c;
   msg.rfid[3] = d;
-  esp_now_send(macAddr, (uint8_t *) &msg, sizeof(msg));
+
+  Serial.print("Sending ");
+  Serial.print(msg.rfid[0], HEX);
+  Serial.print(msg.rfid[1], HEX);
+  Serial.print(msg.rfid[2], HEX);
+  Serial.println(msg.rfid[3], HEX);
+  esp_now_send(NULL, (uint8_t *) &msg, sizeof(msg));
 }
