9d454ee9500aee48de4e9f669ed95fc890febb28
Mike Vanis <hello@mikevanis.com>
2022-02-01 14:24:55 +0000

    added client test

---
+++ Freader_AIO_Client/Freader_AIO_Client.ino
@@ -0,0 +1,33 @@
+#include <espnow.h>
+#include <ESP8266WiFi.h>
+
+typedef struct message_struct {
+  uint8_t rfid[4];
+} message_struct;
+
+message_struct msg;
+
+void setup() {
+  Serial.begin(115200);
+  if (esp_now_init() == ERR_OK) {
+    Serial.println("ESPNow Init Success");
+  }
+  else {
+    Serial.println("ESPNow Init Failed");
+    ESP.restart();
+  }
+}
+
+void loop() {
+  sendTag(random(0, 255), random(0, 255), random(0, 255), random(0, 255));
+  delay(10000);
+}
+
+void sendTag(uint8_t a, uint8_t b, uint8_t c, uint8_t d) {
+  uint8_t macAddr[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+  msg.rfid[0] = a;
+  msg.rfid[1] = b;
+  msg.rfid[2] = c;
+  msg.rfid[3] = d;
+  esp_now_send(macAddr, (uint8_t *) &msg, sizeof(msg));
+}
